d3ae9521c1cf2c98af473e17cb7500487045bd51
bestalexlogin <never.fade.ever@gmail.com>
2023-11-13 16:42:57 +0200

    commit

---
+++ js/01-gallery.js
@@ -0,0 +1,53 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ original, preview, description }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"/>
+        </a>
+    </li>`
+    )
+    .join("");
+}
+
+refs.gallery.innerHTML = createGalleryMarkup(galleryItems);
+refs.gallery.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+
+  const { target } = evt;
+  if (!target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const largeImageUrl = target.dataset.source;
+  const alt = target.alt;
+  const instance = basicLightbox.create(
+    `<img src="${largeImageUrl}" alt="${alt}">`,
+    {
+      onShow: () => document.addEventListener("keydown", onKeyPress),
+      onClose: () => document.removeEventListener("keydown", onKeyPress),
+    }
+  );
+
+  instance.show();
+
+  function onKeyPress(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,26 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ original, preview, description }) => `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+    )
+    .join("");
+}
+
+refs.gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
